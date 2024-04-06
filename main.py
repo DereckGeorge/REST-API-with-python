5640d5bbc8a6a6b288f7408d86cf836049ab687b
DereckGeorge <eridericgeorge@gmail.com>
2024-04-06 19:11:18 +0300

    GET request with passed parameter

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+cars = {"BMW":{"type":"automatic","fuel":"diesel"},
+        "RANGEROVER":{"type":"manual","fuel":"petrol"}}
+
 class HellowWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self,name):
+        return cars[name]
     
-api.add_resource(HellowWorld,"/helloworld")
+    
+api.add_resource(HellowWorld,"/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
